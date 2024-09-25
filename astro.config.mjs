398bd6febbf2e281897f3710d3ac90529917cb0a
hizzybabe <napipat_s@hotmail.com>
2024-09-25 13:54:23 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,6 +15,7 @@ export default defineConfig({
 					items: [
 						// Each item here is one entry in the navigation menu.
 						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'U.S. Dividend Investing Handbook', slug: 'guides/div-etf-101-reddit' },
 					],
 				},
 				{
