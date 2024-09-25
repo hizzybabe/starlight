68a14cc180230c69be656a7602721482c9152cff
hizzybabe <napipat_s@hotmail.com>
2024-09-25 14:05:32 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'ETF 101 to Pro Wiki', slug: 'guides/example' },
 						{ label: 'U.S. Dividend Investing Handbook', slug: 'guides/div-etf-101-reddit' },
 						{ label: 'ประเภทของสินทรัพย์และการลงทุน', slug: 'guides/asset-types' },
 						{ label: 'ลงทุน U.S. ETF ผ่าน Dime!', slug: 'guides/dime' },
