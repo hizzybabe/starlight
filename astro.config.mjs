1b698442e67cd56d693fa4e6bc0ecc9e225a1014
hizzybabe <napipat_s@hotmail.com>
2024-10-05 19:46:37 +0700

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,30 +3,37 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'ETF Wiki',
-			social: {
-				github: 'https://github.com/hizzybabe/starlight',
-			},
-			sidebar: [
-				{
-					label: 'ETF',
-					items: [
-						{ slug: 'guides/example' },
-						{ slug: 'guides/asset-types' },
-						{ slug: 'guides/whats-etf-lemon8' },
-						{ slug: 'guides/div-etf-101-reddit' },
-						{ slug: 'guides/div-etf-101-reddit-lite' },
-						{ slug: 'guides/dime' },
-						{ slug: 'guides/innovestx' },
-					  ],
-					},
-				{
-					label: 'Gold and Bitcoin',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'ETF Wiki',
+      social: {
+        github: 'https://github.com/hizzybabe/starlight',
+      },
+      sidebar: [
+        {
+          label: 'ETF',
+          items: [
+            { slug: 'guides/example' },
+            { slug: 'guides/asset-types' },
+            { slug: 'guides/whats-etf-lemon8' },
+            { slug: 'guides/div-etf-101-reddit' },
+            { slug: 'guides/div-etf-101-reddit-lite' },
+            { slug: 'guides/dime' },
+            { slug: 'guides/innovestx' },
+          ],
+        },
+        {
+          label: 'Gold and Bitcoin',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
+  vite: {
+    resolve: {
+      alias: {
+        '@assets': '/src/assets',  // Add this line
+      },
+    },
+  },
 });
--- src/content/docs/guides/whats-etf-lemon8.md
+++ src/content/docs/guides/whats-etf-lemon8.md
@@ -2,7 +2,7 @@
 title: ETF คืออะไร จบใน 5 slide
 description: ETF คืออะไร จบใน 5 slide (จาก platform Lemon8 ของทาง Evolve Us)
 ---
-![ETF คืออะไร จบใน 5 slide](/src/assets/lemon8pic/4.png)
-![ETF คืออะไร จบใน 5 slide](/src/assets/lemon8pic/5.png)
-![ETF คืออะไร จบใน 5 slide](/src/assets/lemon8pic/6.png)
-![ETF คืออะไร จบใน 5 slide](/src/assets/lemon8pic/7.png)
+![ETF คืออะไร จบใน 5 slide](@assets/lemon8pic/4.png)
+![ETF คืออะไร จบใน 5 slide](@assets/lemon8pic/5.png)
+![ETF คืออะไร จบใน 5 slide](@assets/lemon8pic/6.png)
+![ETF คืออะไร จบใน 5 slide](@assets/lemon8pic/7.png)
